8d1cd83e404a20515e7770abc32f1393caeeb21f
Alberto Roldán <a-f-r@hotmail.es>
2021-02-25 22:23:23 +0100

    Create components folder, App component and import it on index.js

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+
+
+import React from "react";
+
+function App() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+export default App;
+
