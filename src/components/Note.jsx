d574f0576d4d8d470e96dbe61165f4042729d225
Alberto Roldán <a-f-r@hotmail.es>
2021-02-25 23:26:56 +0100

    Add div with a class of note, an h1 and p inside it

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Note() {
   return (
-    
+    <div className="note">
+        <h1>This is a note title</h1>
+        <p>This is the note content</p>
+    </div>
 
   );
 }
