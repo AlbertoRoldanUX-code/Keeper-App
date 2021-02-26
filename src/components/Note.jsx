f2e5d8d1ba01a0cd729b8a6da7903af0649e325e
Alberto Roldán <a-f-r@hotmail.es>
2021-02-26 20:34:50 +0100

    Create note.js file and notes array inside it

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Note() {
   return (
     <div className="note">
-        <h1>This is a note title</h1>
+        <h1>This is the note title</h1>
         <p>This is the note content</p>
     </div>
 
